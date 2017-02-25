763eafcdc36ac926915d8680926178d7a5bf7011
Jefferson-Faseler <jjfaseler@gmail.com>
2017-02-25 14:56:40 -0500

    Working turns for game. Restructured responsibility of functions and tasks.

--- src/App.js
+++ src/App.js
@@ -12,33 +12,36 @@ class Board extends React.Component {
   constructor() {
     super()
     this.state = {
-      squares: [0,0,0,0,0,0,0,0,0]
+      squares: Array(9).fill(0),
+      symbols: Array(9).fill(null)
     }
     this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick(event) {
+    if (event.target.textContent === '' ) {
+      event.target.textContent = this.props.xIsNext ? 'X' : 'O';
+    }
     this.props.onEachClick()
   }
 
   render() {
-    var symbol = this.props.xIsNext ? 'X' : 'O';
     return (
       <div>
         <div>
-        <Square value={this.state.squares[0]} onClick={this.handleClick} symbol={symbol}/>
-        <Square value={this.state.squares[1]} onClick={this.handleClick}/>
-        <Square value={this.state.squares[2]} onClick={this.handleClick}/>
+        <Square value={this.state.squares[0]} onClick={this.handleClick} symbol={this.state.symbols[0]}/>
+        <Square value={this.state.squares[1]} onClick={this.handleClick} symbol={this.state.symbols[1]} />
+        <Square value={this.state.squares[2]} onClick={this.handleClick} symbol={this.state.symbols[2]} />
         </div>
         <div>
-        <Square value={this.state.squares[3]} onClick={this.handleClick}/>
-        <Square value={this.state.squares[4]} onClick={this.handleClick}/>
-        <Square value={this.state.squares[5]} onClick={this.handleClick}/>
+        <Square value={this.state.squares[3]} onClick={this.handleClick} symbol={this.state.symbols[3]} />
+        <Square value={this.state.squares[4]} onClick={this.handleClick} symbol={this.state.symbols[4]} />
+        <Square value={this.state.squares[5]} onClick={this.handleClick} symbol={this.state.symbols[5]} />
         </div>
         <div>
-        <Square value={this.state.squares[6]} onClick={this.handleClick}/>
-        <Square value={this.state.squares[7]} onClick={this.handleClick}/>
-        <Square value={this.state.squares[8]} onClick={this.handleClick}/>
+        <Square value={this.state.squares[6]} onClick={this.handleClick} symbol={this.state.symbols[6]} />
+        <Square value={this.state.squares[7]} onClick={this.handleClick} symbol={this.state.symbols[7]} />
+        <Square value={this.state.squares[8]} onClick={this.handleClick} symbol={this.state.symbols[8]} />
         </div>
       </div>
     )
@@ -61,7 +64,9 @@ class App extends Component {
     } else {
       // logic for computer
     }
-    this.setState({xIsNext: !this.state.xIsNext})
+    let turn = !this.state.xIsNext
+    this.setState({xIsNext: turn})
+    debugger
   }
 
   render() {
@@ -71,7 +76,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Play tic tac toe</h2>
         </div>
-        <Board onEachClick={this.eachClick} twoPlayer={this.state.twoPlayer}/>
+        <Board onEachClick={this.eachClick} twoPlayer={this.state.twoPlayer} xIsNext={this.state.xIsNext}/>
       </div>
     );
   }
