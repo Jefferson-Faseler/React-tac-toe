b23b534f54c021b02c4951f7785641fbdba2abc6
Jefferson-Faseler <jjfaseler@gmail.com>
2017-02-25 10:39:27 -0500

    Fix typo in App. Board logic runs, develoing square.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+class Board extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      squares: [0,0,0,0,0,0,0,0,0]
+    }
+  }
+
+  handleClick() {
+    event.preventDefault()
+
+  }
+
+  render() {
+    return (
+      <div>
+      {
+        this.state.squares.map((square, i) => {
+          return (
+            <Square key={i} />
+          )
+        })
+      }
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Play tic tac toe</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Board />
       </div>
     );
   }
