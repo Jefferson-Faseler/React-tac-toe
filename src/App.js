6d1e55a145d2f26f18b62a0f8468bc48c5ec5d25
Jefferson-Faseler <jjfaseler@gmail.com>
2017-02-25 21:52:24 -0500

    Adjust header styling.

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Play tic tac toe</h2>
+          <h2 className="header-title">Play tic tac toe</h2>
         </div>
         <div>
           <Board
