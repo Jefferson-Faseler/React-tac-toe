45e413f8f25db567da254eda31715a9ba1056918
Jefferson Faseler <jjfaseler@gmail.com>
2017-03-01 10:58:09 -0500

    Choose symbol (#7)
    * User can succesfully choose O or X.
    
    * Changed controls to links for hover effect.
    
    * Added hover effect to controls.
    
    * Remove symbol choice after game has started.

--- src/Board.js
+++ src/Board.js
@@ -24,6 +24,8 @@ class Board extends React.Component {
     this.minimax = this.minimax.bind(this)
     this.placeMark = this.placeMark.bind(this)
     this.resetGame = this.resetGame.bind(this)
+    this.startX = this.startX.bind(this)
+    this.startO = this.startO.bind(this)
   }
 
   threeInRow(squares, turn) {
@@ -123,6 +125,14 @@ class Board extends React.Component {
     this.checkForWin(symbol)
   }
 
+  startX() {
+    this.setState({turn: 'X'})
+  }
+
+  startO() {
+    this.setState({turn: 'O'})
+  }
+
   // handles most functionality of app
   // finds if the square clicked is empty, if it is then
   // appropriate symbol is placed in square and then
@@ -166,10 +176,22 @@ class Board extends React.Component {
   }
 
   render() {
+    var chooseSymbol
+
+    if (!this.state.squares.includes('X') && !this.state.squares.includes('O')) {
+      chooseSymbol = (
+      <div>
+        <a href="javascript:void(0)" onClick={this.startX} className="header-title controls">Play as X</a>
+        <a href="javascript:void(0)" onClick={this.startO} className="header-title controls">Play as O</a>
+      </div>
+      )
+    }
+
     return (
       <div>
-        <button onClick={this.activateAI} className="header-title controls">Activate unbeatable AI</button>
-        <button onClick={this.resetGame} className="header-title controls">Reset Game</button>
+        <a href="javascript:void(0)" onClick={this.activateAI} className="header-title controls">Activate unbeatable AI</a>
+        <a href="javascript:void(0)" onClick={this.resetGame} className="header-title controls">Reset Game</a>
+        {chooseSymbol}
         <div id="game-board">
         <h1 className="header-title game-message">{this.state.message}</h1>
           <div className={"board-row"}>
